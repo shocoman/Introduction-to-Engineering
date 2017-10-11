0be9484df6cc874a9f853a5f369d0dc6434d7335
ivas1ly <ivas1ly@yandex.ru>
2017-10-11 20:13:36 +0700

    Второй коммит на главную ветку

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 #include "math.h"
 
 int main() {
-    float a = 0.0, b = 0.0, s = 0.0;
+    float a = 0.0, b = 0.0, s = 0.0, p = 0.0;
 
     printf("Enter the dimensions of the rectangle: ");
     scanf("%f", &a);
     scanf("%f", &b);
 
     s = a * b;
+    p = 2.0 * (a + b);
 
-    printf("\n\nArea of the rectangle = %f", s);
+    printf("\nArea of the rectangle = %f", s);
+    printf("\nPerimeter of the rectangle = %f", p);
 
     getch();
     return 0;
