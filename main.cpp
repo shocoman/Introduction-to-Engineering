7c02522ce5547206f9332e6711b8d345913fb6f8
ivas1ly <ivas1ly@yandex.ru>
2017-10-11 21:35:30 +0700

    Изменение комментария для создания конфликта

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@
 int main() {
     float a = 0.0, b = 0.0, s = 0.0, p = 0.0;
 
-    printf("Enter the dimensions of the rectangle: ");
-    scanf("%f", &a);
+    printf("Enter the dimensions of the rectangle: "); //Делаем запрос 1
+    scanf("%f", &a);                                   //Считываем данные с клаваиатуры 2
     scanf("%f", &b);
 
-    s = a * b;
+    s = a * b;                                          //Делаем расчёты 3
     p = 2.0 * (a + b);
 
     printf("\nArea of the rectangle = %f", s);
-    printf("\nPerimeter of the rectangle = %f", p);
+    printf("\nPerimeter of the rectangle = %f", p);     //Производим вывод результата
+
 
     getch();
     return 0;
